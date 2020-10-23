c980269f1a9c67fff63916cf8e4af492755ec0bb
rodion-s <rodion13par@gmail.com>
2020-10-23 15:46:27 +0300

    hw2 GradientBoostingImpl added

---
+++ Unit1/Hw2_Ensembles/_predict.c
@@ -0,0 +1,29 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <memory.h>
+int __new_predict(
+    float *x, 
+    int len, 
+    int num_features,
+    int *is_leafs, 
+    int *feature_ids, 
+    float *thresholds, 
+    float *answers_in_leaf,
+    float *predictions,
+    int *leaf_indices)
+{
+    for (int i = 0; i < len; ++i) {
+        int node_id = 0;
+        while (is_leafs[node_id] == 0) {
+            if (x[num_features * i + feature_ids[node_id]] <= thresholds[node_id]) {
+                node_id = 2 * node_id + 1;
+            }
+            else {
+                node_id = 2 * node_id + 2;
+            }
+        }
+        predictions[i] = answers_in_leaf[node_id];
+        leaf_indices[i] = node_id;
+    }
+    return 0;
+}
